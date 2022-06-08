f2cf2813a88073b4a533c34d5ab1a331b8dbed27
JoeyM <joeymarino94@gmail.com>
2022-06-08 14:04:10 -0600

    make simple NFT contract with Open Zeppelin library

---
+++ nft-mrkt-backend/contracts/Oznft.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Oznft is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("OzExampleNft", "ONFT") {}
+
+    function mintToken(address to) public returns(uint256) {
+        _tokenIds.increment();
+        _mint(to, _tokenIds.current());
+        return _tokenIds.current();
+    }
+}
