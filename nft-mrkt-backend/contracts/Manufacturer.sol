a2c286dcb225a331e28f391f3829611fbbe34289
JoeyM <joeymarino94@gmail.com>
2022-06-17 16:07:19 -0600

    make manufacturer contract and test suite for it

--- nft-mrkt-backend/contracts/Manufacturer.sol
+++ nft-mrkt-backend/contracts/Manufacturer.sol
@@ -1,9 +1,52 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
+import "./Oznft.sol";
+
 contract Manufacturer {
 
-    constructor() {}
+    address[] admins;
+    address[] contracts;
+    string companyName;
+    string logoIpfsUrl;
 
+    modifier onlyAdmin() {
+        bool isAdmin = false;
 
+        for(uint i=0; i<admins.length; i++) {
+            if(msg.sender == admins[i]) {
+                isAdmin = true;
+                break;
+            }
+        }
+        require(isAdmin == true, "Only Admins can do this!");
+        _;
+    }
+    constructor() {
+        admins.push(msg.sender);
+        // These will obviously be made into parameters
+        companyName = "Wonka Industries";
+        logoIpfsUrl = "https://gateway.pinata.cloud/ipfs/QmYxT4LnK8sqLupjbS6eRvu1si7Ly2wFQAqFebxhWntcf6/";
+    }
+
+    function addAdmin(address _newAdmin) public onlyAdmin {
+        admins.push(_newAdmin);
+    }
+
+    function getAdmin(uint256 _index) public view returns(address) {
+        return(admins[_index]);
+    }
+
+    function getAdminCount() public view returns(uint256) {
+        return admins.length;
+    }
+    
+    function addContract(address _address) public onlyAdmin {
+        contracts.push(_address);
+    }
+
+    function getContractCount() public view returns(uint256) {
+        return contracts.length;
+    }
 }
---
+++ nft-mrkt-backend/test/ManufacturerTests.ts
@@ -0,0 +1,82 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe.only("Manufacturer Contract", function () {
+  let owner: any;
+  let user1: any;
+  let user2: any;
+  let ManufacturerContract: any;
+  let OzContract: any;
+  let OzContract2: any;
+
+  before(async function () {
+    [owner, user1, user2] = await ethers.getSigners();
+
+    const ManufacturerContract_ = await ethers.getContractFactory(
+      "Manufacturer"
+    );
+    ManufacturerContract = await ManufacturerContract_.deploy();
+    await ManufacturerContract.deployed();
+  });
+  describe("Admins", async function () {
+    it("should have one admin", async function () {
+      const adminCount = await ManufacturerContract.getAdminCount();
+      await expect(adminCount).to.equal(1);
+    });
+    it("should have added an admin", async function () {
+      // eslint-disable-next-line prettier/prettier
+      const tx = await ManufacturerContract.connect(owner).addAdmin(user1.address);
+      await tx.wait();
+      const adminCount = await ManufacturerContract.getAdminCount();
+      await expect(adminCount).to.equal(2);
+    });
+  });
+  describe("contracts", async function () {
+    it("should mint an NFT contract", async function () {
+      const OzContract_ = await ethers.getContractFactory("OzNFT");
+      OzContract = await OzContract_.connect(user1).deploy(
+        "WonkaChocolate",
+        "WNKAC"
+      );
+      await OzContract.deployed();
+    });
+    it("should add that NFT contract to the Manufacturer's db", async function () {
+      const contractAddress = OzContract.address;
+      const tx2 = await ManufacturerContract.addContract(contractAddress);
+      await tx2.wait();
+      const contractCount = await ManufacturerContract.getContractCount();
+      await expect(contractCount).to.equal(1);
+    });
+    it("should mint another NFT contract", async function () {
+      const OzContract_ = await ethers.getContractFactory("OzNFT");
+      OzContract2 = await OzContract_.connect(user1).deploy(
+        "WonkaGum",
+        "WNKAG"
+      );
+      await OzContract2.deployed();
+    });
+    it("should add the second NFT contract to the Manufacturer's db", async function () {
+      const contract2Address = OzContract2.address;
+      const tx3 = await ManufacturerContract.addContract(contract2Address);
+      await tx3.wait();
+      const contractCount = await ManufacturerContract.getContractCount();
+      await expect(contractCount).to.equal(2);
+    });
+  });
+  describe("minting and transfer", async function () {
+    it("a buyer should be able to lazy mint from contract 2", async function () {
+      const tx4 = await OzContract2.connect(user2).mintToken(user2.address);
+      await tx4.wait();
+      const user2Contract2Balance = await OzContract2.balanceOf(user2.address);
+      await expect(user2Contract2Balance).to.equal(1);
+    });
+    it("a buyer should be able to transfer that token", async function () {
+      const tx5 = await OzContract2.connect(user2).transfer(user1.address, 1);
+      await tx5.wait();
+      const user2Contract2Balance = await OzContract2.balanceOf(user2.address);
+      const user1Contract2Balance = await OzContract2.balanceOf(user1.address);
+      await expect(user2Contract2Balance).to.equal(0);
+      await expect(user1Contract2Balance).to.equal(1);
+    });
+  });
+});
