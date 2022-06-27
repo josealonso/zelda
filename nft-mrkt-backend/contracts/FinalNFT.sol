0b91f8a0a3b5ad2171d2497b987d009101a12351
José Ramón (JR) <jose.alonso.programmer@gmail.com>
2022-06-27 20:57:11 +0200

    Update FinalNFT.sol

--- nft-mrkt-backend/contracts/FinalNFT.sol
+++ nft-mrkt-backend/contracts/FinalNFT.sol
@@ -6,7 +6,11 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract MyNFT is ERC721URIStorage {
     uint256 public tokenCount;
 
-    constructor() ERC721("name", "SYM") {}
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {
+        // more ERC721 variables can be initialized here
+    }
 
     function mint(string memory _tokenURI) external returns (uint256) {
         tokenCount++;
