154ec346309d3bc3f3d5006a9a0f75395b60c719
JoeyM <joeymarino94@gmail.com>
2022-06-27 11:45:25 -0600

    clean up MM display. Add MM logo

--- nft-mrkt-frontend/src/components/Navbar/Navbar.tsx
+++ nft-mrkt-frontend/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './navbar.scss'
 import { Link } from "react-router-dom";
 import { ethers } from "ethers";
 import Web3Modal from "web3modal";
+import Metamask from "../Assets/Metamask.svg";
 
 import { useStore } from "../../userStore";
 // import {JsonRpcSigner, Web3Provider} from "@ethersproject/providers";
@@ -13,25 +14,33 @@ const { ethereum } = window as any;
 const Navbar: React.FC = () => {
 
   const { user, setAddress } = useStore();
+  const [dispAddr, setDispAddr] = useState<string>("");
 
+  useEffect(() => {
+    async function setInfo() {
 
+    }
+    setInfo();
+  }, [user])
+
+  function makeDispAddr(numAddr: string) {
+    const strAddr = numAddr.toString();
+      const first = strAddr.slice(0,4);
+      const last = strAddr.slice(-4);
+      const dispAddr = `${first}...${last}`;
+      setDispAddr(dispAddr);
+  }
+ 
   async function connectMM() {
     if(ethereum) {
       const web3Modal = new Web3Modal()
       const prov = new ethers.providers.Web3Provider(await web3Modal.connect())
-      // setState((prevState: any) => ({
-      //   ...prevState,
-      //   provider: prov
-      // }));
       const signer = await prov.getSigner()
       const address = await signer.getAddress();
-      console.log("address is: ", address,);
-      console.log("Address is of type: ", typeof address);
+      // console.log("address is: ", address,);
+      // console.log("Address is of type: ", typeof address);
       setAddress(address);
-      // setState((prevState: any) => ({
-      //   ...prevState,
-      //   polyAddr: address
-      // }));
+      makeDispAddr(address);
     }
   }
 
@@ -42,8 +51,12 @@ const Navbar: React.FC = () => {
       </div>
       <div className='right'>
         <Link to="/consumer" className='consumerLink'> My Items </Link>
-        {user?.addrString ? <button className='connectMMBtn'>{user.addrString}</button> :
-                <button className='connectMMBtn' onClick={connectMM}>Connect Wallet</button>
+        {user?.addrString ? 
+          <button className='connectMMBtn'>
+            <img src={Metamask} alt="metamask logo" ></img>
+            {dispAddr}
+          </button> :
+          <button className='connectMMBtn' onClick={connectMM}>Connect Wallet</button>
         }
       </div>
     </div>
