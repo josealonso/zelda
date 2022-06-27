83ffdc19de05790ed26a12d724b4b00daf9a882f
José Ramón (JR) <jose.alonso.programmer@gmail.com>
2022-06-27 21:06:06 +0200

    Create FinalCollection.sol

---
+++ nft-mrkt-backend/contracts/FinalCollection.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFTCollection is ERC721URIStorage {
+    uint256 public tokenCount;
+
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {
+        // more ERC721 variables can be initialized here
+    }
+
+    function mint(string memory _tokenURI) external returns (uint256) {
+        tokenCount++;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        return tokenCount;
+    }
+}
