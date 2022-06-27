885c4b485f3d499a9271aa7ca899924fcd1ed3fa
Junyuan Lau <junyuan@Junyuans-New-MBP.local>
2022-06-26 22:23:37 -0700

    Add backend implementation
    -- add actual implementation for Backend interface which can
    read from contracts

--- nft-mrkt-frontend/src/api/BackendImpl.test.ts
+++ nft-mrkt-frontend/src/api/BackendImpl.test.ts
@@ -1,11 +1,11 @@
 import '@testing-library/jest-dom'
-import BackendImpl from "./BackendImpl";
+import BackendAPIImpl from "./BackendImpl";
 
 const testAddress = "0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de"
 
 test('covalent api', async () => {
-    let api = new BackendImpl()
+    let api = new BackendAPIImpl("test")
     let nfts = await api.getUserNFTs(testAddress)
     expect(nfts).not.toBeNull()
     expect(nfts).toHaveLength(4)
-}, 10000);
+}, 15000);
