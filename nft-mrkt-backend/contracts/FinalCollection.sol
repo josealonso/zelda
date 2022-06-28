7f622dd02c86244c00513e926cdf57b4fea8438d
José Ramón (JR) <jose.alonso.programmer@gmail.com>
2022-06-28 03:06:47 +0200

    Update FinalCollection.sol
    Defined more functions

--- nft-mrkt-backend/contracts/FinalCollection.sol
+++ nft-mrkt-backend/contracts/FinalCollection.sol
@@ -4,11 +4,21 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFTCollection is ERC721URIStorage {
-    uint256 public tokenCount;
+    uint256 public maxNumOfItems;
+    string public productName;
+    address public makerAddress;
+    uint256 public tokenCount; // counter
 
-    constructor(string memory _name, string memory _symbol)
-        ERC721(_name, _symbol)
-    {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _maxNumOfItems,
+        string memory _productName,
+        address _makerAddress
+    ) ERC721(_name, _symbol) {
+        maxNumOfItems = _maxNumOfItems;
+        productName = _productName;
+        makerAddress = _makerAddress;
         // more ERC721 variables can be initialized here
     }
 
@@ -17,5 +27,21 @@ contract NFTCollection is ERC721URIStorage {
         _safeMint(msg.sender, tokenCount);
         _setTokenURI(tokenCount, _tokenURI);
         return tokenCount;
+    }
+
+    function getNumOfCollectionItems() external view returns (uint256) {
+        return maxNumOfItems;
+    }
+
+    function getCollectionAddress() external view returns (address) {
+        return address(this);
+    }
+
+    function getMakerAddress() external view returns (address) {
+        return makerAddress;
+    }
+
+    function getProductName() external view returns (string memory) {
+        return productName;
     }
 }
