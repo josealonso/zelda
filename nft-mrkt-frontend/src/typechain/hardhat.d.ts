3f7fddf7c646e11aa26a25c2182f4165f448a982
Junyuan Lau <junyuan.lau@gmail.com>
2022-06-28 08:02:55 -0700

    Update generated artifact and typechain code (#35)
    Co-authored-by: Junyuan Lau <junyuan@Junyuans-New-MBP.local>

--- nft-mrkt-frontend/src/typechain/hardhat.d.ts
+++ nft-mrkt-frontend/src/typechain/hardhat.d.ts
@@ -57,10 +57,6 @@ declare module "hardhat/types/runtime" {
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.NFTCollection__factory>;
     getContractFactory(
-      name: "FinalMarketplace",
-      signerOrOptions?: ethers.Signer | FactoryOptions
-    ): Promise<Contracts.FinalMarketplace__factory>;
-    getContractFactory(
       name: "FinalMarketplaceV2",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.FinalMarketplaceV2__factory>;
@@ -144,11 +140,6 @@ declare module "hardhat/types/runtime" {
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.NFTCollection>;
-    getContractAt(
-      name: "FinalMarketplace",
-      address: string,
-      signer?: ethers.Signer
-    ): Promise<Contracts.FinalMarketplace>;
     getContractAt(
       name: "FinalMarketplaceV2",
       address: string,
