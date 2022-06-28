cf0dc6ee3f674279177d01f476196a745fdfad96
JoeyM <joeymarino94@gmail.com>
2022-06-28 11:14:58 -0600

    update import

--- nft-mrkt-frontend/src/components/Manufacturer/Modal/Modal.tsx
+++ nft-mrkt-frontend/src/components/Manufacturer/Modal/Modal.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import styles from "./Modal.module.scss";
 import { RiCloseLine } from "react-icons/ri";
-import { useStore } from "../../../userStore";
+import { useStore } from "../../../Store/userStore";
 import { GetInstance } from "../../../api/BackendIf";
 import Confirm from "./Confirm";
 
