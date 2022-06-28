855104517b7be018f2618b7502bb32af6088d7b6
Junyuan Lau <junyuan@Junyuans-New-MBP.local>
2022-06-28 07:58:33 -0700

    Generate code for new contracts

--- nft-mrkt-frontend/src/typechain/hardhat.d.ts
+++ nft-mrkt-frontend/src/typechain/hardhat.d.ts
@@ -53,6 +53,18 @@ declare module "hardhat/types/runtime" {
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.IERC165__factory>;
     getContractFactory(
+      name: "NFTCollection",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.NFTCollection__factory>;
+    getContractFactory(
+      name: "FinalMarketplace",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.FinalMarketplace__factory>;
+    getContractFactory(
+      name: "FinalMarketplaceV2",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.FinalMarketplaceV2__factory>;
+    getContractFactory(
       name: "Manufacturer",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.Manufacturer__factory>;
@@ -127,6 +139,21 @@ declare module "hardhat/types/runtime" {
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.IERC165>;
+    getContractAt(
+      name: "NFTCollection",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.NFTCollection>;
+    getContractAt(
+      name: "FinalMarketplace",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.FinalMarketplace>;
+    getContractAt(
+      name: "FinalMarketplaceV2",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.FinalMarketplaceV2>;
     getContractAt(
       name: "Manufacturer",
       address: string,
