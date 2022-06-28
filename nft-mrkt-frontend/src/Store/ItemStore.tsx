ec213e14a7535fe00afd686b00042c4903aa3146
JoeyM <joeymarino94@gmail.com>
2022-06-28 11:16:07 -0600

    make item store

---
+++ nft-mrkt-frontend/src/Store/ItemStore.tsx
@@ -0,0 +1,47 @@
+import create from "zustand";
+
+import { Item } from "../models/Item";
+
+interface ItemState {
+    item: Item
+    setItem: (
+        contractAddress: string, 
+        tokenId: string, 
+        forSale: boolean, 
+        price: number, 
+        name: string, 
+        productUri: string | undefined
+    ) => void
+}
+
+export const itemStore = create<ItemState>((set) => ({
+    
+    item: {
+        contractAddress: "",
+        tokenId: "",
+        forSale: false,
+        price: 0,
+        name: "",
+        productUri: ""
+    },
+    
+    setItem: (
+            _contractAddress: string, 
+            _tokenId: string, 
+            _forSale: boolean, 
+            _price: number, 
+            _name: string, 
+            _productUri: string | undefined
+        ) => {
+        set(() => ({
+            item: {
+                contractAddress: _contractAddress, 
+                tokenId: _tokenId, 
+                forSale: _forSale, 
+                price: _price, 
+                name: _name, 
+                productUri: _productUri
+            }
+        }))
+    }
+}))
