246b189b31f13eb9b7024156940e2ef29ce4be7a
Junyuan Lau <junyuan@Junyuans-New-MBP.local>
2022-06-28 14:42:06 -0700

    Remove console log env

--- nft-mrkt-frontend/src/components/UploadImageForm/UploadImageForm.tsx
+++ nft-mrkt-frontend/src/components/UploadImageForm/UploadImageForm.tsx
@@ -15,8 +15,6 @@ function UploadImageForm() {
         }
         console.log("We have found a file", fileImg)
         try {
-            console.log("various variables", process.env)
-
             const api_key = process.env.REACT_APP_PINATA_API_KEY
             const api_secret_key = process.env.REACT_APP_PINATA_API_SECRET_KEY
 
