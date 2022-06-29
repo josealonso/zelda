225d97a532df42b2e36d83faef623591c15500b9
José Ramón (JR) <jose.alonso.programmer@gmail.com>
2022-06-29 14:25:10 +0200

    Update deployCollection.ts
    Added verification process in the script. This can save a lot of time if a number of different collections are to be deployed.
    Note that the same contract can only be verified once, although it can be deployed as many times as you want.

--- nft-mrkt-backend/scripts/deployCollection.ts
+++ nft-mrkt-backend/scripts/deployCollection.ts
@@ -5,6 +5,12 @@ async function main() {
   // URL from where we can extract the metadata for the NFTs
   const metadataURL = "ipfs://QmZc7jp32oZNbaSGBLbMib4GU5YnGesvudHcE3o7EVqdjs/";
   const MY_ADDRESS = "0xFE2de2924c17C5A5E351E5fD13E2657836716BdD";
+  const COLLECTION_NAME = "Wood Tables";
+  const COLLECTION_SYMBOL = "WTB";
+  const COLLECTION_MAX_NUM_OF_ITEMS = 40;
+  const COLLECTION_PRODUCT_NAME = "Tables";
+  const COLLECTION_MAKER_ADDRESS = MY_ADDRESS;
+  const WAITING_UNTIL_DEPLOYMENT = 60000;   // In miliseconds
   /*
   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
   so collectionContract here is a factory for instances of our NFTCollection contract.
@@ -16,6 +22,16 @@ async function main() {
   await deployedCollectionContract.deployed();
   // print the address of the deployed contract
   console.log("Collection Contract Address:", deployedCollectionContract.address);
+
+  // Verify the contract
+  const waitFor = (delay: number) => new Promise(resolve => setTimeout(() => {
+    hre.run("verify:verify", {
+      address: deployedCollectionContract.address,
+      constructorArguments: [COLLECTION_NAME, COLLECTION_SYMBOL, COLLECTION_MAX_NUM_OF_ITEMS,
+        COLLECTION_PRODUCT_NAME, COLLECTION_MAKER_ADDRESS],
+    })
+  }, delay));
+  await waitFor(WAITING_UNTIL_DEPLOYMENT);
 
   // To be changed
   writeFileSync('../frontend/config.ts', `export const marketplaceAddress = "${deployedCollectionContract.address}"
