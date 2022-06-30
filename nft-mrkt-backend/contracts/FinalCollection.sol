b4d5f37f2bbdcf344ac383d6bf4553db5d0c54e2
JoeyM <joeymarino94@gmail.com>
2022-06-30 11:54:30 -0600

    update NFT contract to reflect everything we will need for the frontend

--- nft-mrkt-backend/contracts/FinalCollection.sol
+++ nft-mrkt-backend/contracts/FinalCollection.sol
@@ -1,31 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract NFTCollection is ERC721URIStorage {
+contract NFTCollection is ERC721 {
     uint256 public maxNumOfItems;
     string public productName;
     address public makerAddress;
     uint256 public tokenCount; // counter
+
+    /* 
+        Sale price needs to be defined to work with frontend
+        See frontend>src>api>Backend.if>FinalNFTContract type
+    */
+    uint256 public makerSalePrice;
+
+    /* 
+        Product Uri should be set for the contract, not the NFT's.
+        Product lines are fungible until first sale.
+    */
+    string public productUri;
+
+    /* 
+        Product metadata needs to be included to function with optional
+        fields that we allow to input on contract creation modal
+    */
+    string public productMeta;
 
     constructor(
         string memory _name,
         string memory _symbol,
         uint256 _maxNumOfItems,
         string memory _productName,
-        address _makerAddress
+        uint256 _makerSalePrice,
+        string memory _productMeta
     ) ERC721(_name, _symbol) {
         maxNumOfItems = _maxNumOfItems;
         productName = _productName;
-        makerAddress = _makerAddress;
-        // more ERC721 variables can be initialized here
+        makerAddress = msg.sender;
+        makerSalePrice = _makerSalePrice;
+        productMeta = _productMeta;
     }
 
-    function mint(string memory _tokenURI) external returns (uint256) {
+    function mint() external returns (uint256) {
         tokenCount++;
         _safeMint(msg.sender, tokenCount);
-        _setTokenURI(tokenCount, _tokenURI);
         return tokenCount;
     }
 
