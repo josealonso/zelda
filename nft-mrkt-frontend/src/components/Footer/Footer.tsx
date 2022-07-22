7c34cf554701805dfc9ad8f714c457bb7e44f38a
JR <jose.alonso.programmer@gmail.com>
2022-07-22 21:12:18 +0200

    Add link to our twitter account

--- nft-mrkt-frontend/src/components/Footer/Footer.tsx
+++ nft-mrkt-frontend/src/components/Footer/Footer.tsx
@@ -4,32 +4,33 @@ import GitHubLogo  from "../Assets/GitHubLogo.png";
 import { Link } from "react-router-dom";
 import React from 'react'
 import "./footer.scss";
+import { Box } from "@chakra-ui/react";
 
 const Footer: React.FC = () => {
   return (
-    <div className='footer' id="footer">
-      <div className='left'>
+    <Box className='footer' id="footer">
+      <Box className='left'>
         <Link to="/maker" className='text'> Makers </Link>
         <Link to="/about" className='text about'> About </Link>
-      </div>
-      <div className='right'>
-        <div className='icon github'>
+      </Box>
+      <Box className='right'>
+        <Box className='icon github'>
           <a href="https://github.com/our3-xyz/NFT-MRKT" className='linkBox' id="linkBox">
             <img src={GitHubLogo} alt="github logo"></img>
           </a>
-        </div>
-        <div className='icon twitter'>
-          <a href="https://github.com/our3-xyz/NFT-MRKT" className='linkBox' id="linkBox">
+        </Box>
+        <Box className='icon twitter'>
+          <a href="https://twitter.com/our310" className='linkBox' id="linkBox">
             <img src={TwitterLogo} alt="twitter logo"></img>
           </a>
-        </div>
-        <div className='icon etherscan'>
+        </Box>
+        <Box className='icon etherscan'>
           <a href="https://github.com/our3-xyz/NFT-MRKT" className='linkBox' id="linkBox">
             <img src={EtherscanLogo} alt="etherscan logo"></img>
           </a>
-        </div>
-      </div>
-    </div>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
